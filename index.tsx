9ccd16bd79231f872b86b61194306a09026fb9a3
Anuj07ku <96259050+Anuj07ku@users.noreply.github.com>
2023-02-08 11:06:37 +0530

    2 files modified, 1 added

--- index.tsx
+++ index.tsx
@@ -3,7 +3,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 //import App from './App';
-import App from './Test';
+import App from './src/Testsrc';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
